d441fd46c8f31938ac3b778ff6fa9c42aa795864
Rimjhim <rimjhimsaxena535@gmail.com>
2025-08-23 22:28:43 +0530

    day22

---
+++ LC49.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (auto &s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); 
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &p : mp) {
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
+};
